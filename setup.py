d7c82cd18fb3c868f95f1d76d17e28779eeb2a0c
Carl Sverre <carl@carlsverre.com>
2013-07-25 10:34:53 -0700

    added dependencies and distribute helper

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!env python
 
-from distutils.core import setup
+from distribute_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 # get version
@@ -19,18 +22,18 @@ class PyTest(TestCommand):
         raise sys.exit(errno)
 
 setup(
-    name='MemSQL',
+    name='memsql',
     version=__version__,    # this is defined by the "exec(f.read())" at the top of this file.
     author='MemSQL',
     author_email='support@memsql.com',
-    packages=[
-        'memsql',
-    ],
     url='http://github.com/memsql/memsql-python',
+
     license='LICENSE.txt',
     description='Useful utilities and plugins for MemSQL integration.',
     long_description=open('README.rst').read(),
-    requires=['wraptor'],
+
+    packages=find_packages(),
+    install_requires=['MySQL-python>=1.2.4', 'wraptor'],
     tests_require=['pytest', 'mock'],
     cmdclass={ 'test': PyTest },
 )
