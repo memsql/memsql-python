bad607ac9e0055ccde465d1c3488c5064b392ca5
Carl Sverre <carl@carlsverre.com>
2013-09-06 16:58:13 -0700

    test watcher, and sub dir support

--- setup.py
+++ setup.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python
-# flake8: noqa
 
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 # get version
-with open('memsql/__init__.py') as f:
-    exec(f.read())
+from memsql import __version__
 
 class PyTest(TestCommand):
+    user_options = [
+        ('watch', 'w',
+         "watch tests for changes"),
+        ('scan-directory=', 'd',
+         "only search for tests in the specified directory"),
+    ]
+    boolean_options = ['watch']
+
+    def initialize_options(self):
+        self.watch = False
+        self.scan_directory = None
+        self.test_suite = None
+        self.test_module = None
+        self.test_loader = None
+
     def finalize_options(self):
         TestCommand.finalize_options(self)
-        self.test_args = []
+
         self.test_suite = True
+        self.test_args = []
+        if self.watch:
+            self.test_args.append('-f')
+        if self.scan_directory is not None:
+            self.test_args.append(self.scan_directory)
 
     def run_tests(self):
-        import pytest, sys
+        import os, sys
+
+        MY_PATH = os.path.dirname(__file__)
+        sys.path.append(MY_PATH)
+        os.environ['PYTHONPATH'] = os.environ.get('PYTHONPATH', '') + ':' + MY_PATH
+
+        import pytest
         errno = pytest.main(self.test_args)
         raise sys.exit(errno)
 
