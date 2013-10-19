f1f51faf4bf9b12c000407ecdca36aa42a19a20a
Carl Sverre <carl@carlsverre.com>
2013-10-18 20:25:06 -0700

    network testing tweaks

--- examples/network_testing.py
+++ examples/network_testing.py
@@ -1,29 +1,36 @@
 from memsql.common import database
 from memsql.perf.network_tester import NetworkTester
 
-with database.connect(host='leaf-1.cs.memcompute.com', user='root') as c:
-    c.execute('CREATE DATABASE IF NOT EXISTS performance')
-    c.execute('SET GLOBAL max_allowed_packet=%d' % (1024 * 1024 * 10))
+master_agg = 'master.cs.memcompute.com'
+test_node = 'leaf-1.cs.memcompute.com'
+iterations = 100
+payload_size = 1024 * 500
 
-n = NetworkTester().connect(host='leaf-1.cs.memcompute.com', user='root', database='performance')
+conn = database.connect(host=master_agg, user='root')
+conn.execute('CREATE DATABASE IF NOT EXISTS performance')
+conn.execute('SET GLOBAL max_allowed_packet=%d' % (1024 * 1024 * 10))
 
-if n.ready():
-    n.destroy()
-if not n.ready():
-    n.setup()
+m = NetworkTester(payload_size=payload_size).connect(host=master_agg, user='root', database='performance')
+if m.ready():
+    m.destroy()
+m.setup()
 
-def pp(data, postfix):
+n = NetworkTester().connect(host=test_node, user='root', database='performance')
+
+def pp(data, postfix, cb=lambda x: x):
     for k, v in data.items():
-        print k, v, postfix
+        print k, cb(v), postfix
 
 print 'latancy'
 pp(n.estimate_latency(), 'ms')
 
-print '\nthroughput'
-pp(n.estimate_throughput(1024 * 1024, 100), 'B/s')
+print '\nroundtrip'
+pp(n.estimate_roundtrip(iterations), 'MB/s', lambda x: (x / 1024 / 1024))
 
 print '\nupload'
-pp(n.estimate_upload(1024 * 1024, 100), 'B/s')
+pp(n.estimate_upload(iterations), 'MB/s', lambda x: (x / 1024 / 1024))
 
 print '\ndownload'
-pp(n.estimate_download(1024 * 1024, 100), 'B/s')
+pp(n.estimate_download(iterations), 'MB/s', lambda x: (x / 1024 / 1024))
+
+conn.execute('DROP DATABASE performance')
--- memsql/perf/network_tester.py
+++ memsql/perf/network_tester.py
@@ -4,14 +4,15 @@ import math
 
 DATA_TABLE = """\
 CREATE /*!90618 REFERENCE */ TABLE IF NOT EXISTS %(name)s (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) PRIMARY KEY,
     data LONGTEXT
 )"""
 
 class NetworkTester(sql_utility.SQLUtility):
-    def __init__(self, table_prefix=''):
+    def __init__(self, table_prefix='network_tester', payload_size=1024 * 100):
         super(NetworkTester, self).__init__()
 
+        self._payload_size = payload_size
         self.table_prefix = table_prefix.rstrip('_')
         self.data_table = self.table_prefix + '_data'
         self._define_table(self.data_table, DATA_TABLE % { 'name': self.data_table })
@@ -19,10 +20,30 @@ class NetworkTester(sql_utility.SQLUtility):
     ###############################
     # Public Interface
 
+    def setup(self):
+        super(NetworkTester, self).setup()
+
+        self.half_payload = 'a' * (self._payload_size / 2)
+        self.payload = self.half_payload * 2
+
+        # bootstrap table
+        with self._db_conn() as conn:
+            try:
+                conn.execute('''
+                    INSERT INTO %s VALUES
+                    ("half_payload", %%s),
+                    ("payload", %%s)
+                ''' % self.data_table, self.half_payload, self.payload)
+            except database.MySQLError as (errno, msg):
+                if errno == errorcodes.ER_DUP_KEY:
+                    pass
+                else:
+                    raise
+
     def estimate_latency(self, iterations=100):
         results = []
         with self._db_conn() as conn:
-            for i in range(iterations):
+            for _ in range(iterations):
                 with timer() as t:
                     conn.ping()
                 results.append(t.interval)
@@ -30,57 +51,49 @@ class NetworkTester(sql_utility.SQLUtility):
         computed = self._compute_results(results)
         return computed
 
-    def estimate_throughput(self, payload_size=1024 * 100, iterations=100):
+    def estimate_roundtrip(self, iterations=100):
         results = []
-        payload = 'a' * (payload_size / 2)
-        delete = 'DELETE FROM %s' % (self.data_table)
-        insert = 'INSERT INTO %s (data) VALUES ("%s")' % (self.data_table, payload)
-        query = 'SELECT data AS a FROM %s LIMIT 1 # %s' % (self.data_table, payload)
+        query = 'SELECT data AS a FROM %s # %%s' % (self.data_table)
         with self._db_conn() as conn:
-            conn.execute(delete)  # ensure that the table is empty
-            conn.execute(insert)  # ensure there is something to select
-            conn.execute(query)  # ensure compile
-            for i in range(iterations):
+            half_payload = conn.get('SELECT data FROM %s WHERE name="half_payload"' % self.data_table).data
+            conn.execute(query, half_payload)  # ensure compile
+
+            for _ in range(iterations):
                 with timer() as t:
-                    conn.query(query)
-                results.append(payload_size / (t.interval / 1000.))
+                    conn.query(query, half_payload)
+                results.append((len(half_payload) * 2) / (t.interval / 1000.))
 
         computed = self._compute_results(results)
         return computed
 
-    def estimate_upload(self, payload_size=1024 * 100, iterations=100):
+    def estimate_upload(self, iterations=100):
         results = []
-        payload = 'a' * (payload_size - 2)
-        query = '!!%s' % (payload)
+        query = '!!%s'
         with self._db_conn() as conn:
-            for i in range(iterations):
+            payload = conn.get('SELECT data FROM %s WHERE name="payload"' % self.data_table).data
+
+            for _ in range(iterations):
                 try:
                     with timer() as t:
-                        conn.query(query)
+                        conn.query(query, payload)
                 except database.MySQLError as (errno, msg):
                     if errno == errorcodes.ER_PARSE_ERROR:
                         pass
                     else:
                         raise
-                results.append(payload_size / (t.interval / 1000.))
+                results.append(len(payload) / (t.interval / 1000.))
 
         computed = self._compute_results(results)
         return computed
 
-    def estimate_download(self, payload_size=1024 * 100, iterations=100):
+    def estimate_download(self, iterations=100):
         results = []
-        payload = 'a' * payload_size
-        delete = 'DELETE FROM %s' % (self.data_table)
-        insert = 'INSERT INTO %s (data) VALUES ("%s")' % (self.data_table, payload)
-        query = 'SELECT data AS a FROM %s LIMIT 1' % (self.data_table)
+        query = 'SELECT data AS a FROM %s WHERE name="payload"' % (self.data_table)
         with self._db_conn() as conn:
-            conn.execute(delete)  # ensure that the table is empty
-            conn.execute(insert)  # ensure there is something to select
-            conn.execute(query)  # ensure compile
-            for i in range(iterations):
+            for _ in range(iterations):
                 with timer() as t:
-                    conn.query(query)
-                results.append(payload_size / (t.interval / 1000.))
+                    payload = conn.get(query).a
+                results.append(len(payload) / (t.interval / 1000.))
 
         computed = self._compute_results(results)
         return computed
